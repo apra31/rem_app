b4da1f63a294fe672c5b5080929c8c73807ecd9d
Unknown <a.pra31@gmail.com>
2019-06-29 15:44:54 +0700

    Update picture page functionality.

---
+++ www/js/picture.js
@@ -0,0 +1,46 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.getElementById('camera-btn').addEventListener('click', this.onCameraButtonClicked.bind(this));
+        document.getElementById('gallery-btn').addEventListener('click', this.onGalleryButtonClicked.bind(this));
+    },
+
+    onCameraButtonClicked: function() {
+        console.log('camera-btn is clicked');
+        navigator.camera.getPicture(onSuccess, onFail, {
+            quality: 50,
+            destinationType: Camera.DestinationType.DATA_URL,
+            correctOrientation: true
+        });
+
+        function onSuccess(imageData) {
+            var image = document.getElementById('pic');
+            image.src = "data:image/jpeg;base64," + imageData;
+        }
+
+        function onFail(message) {
+            alert('Failed because: ' + message);
+        }
+    },
+
+    onGalleryButtonClicked: function() {
+        console.log('gallery-btn is clicked');
+        navigator.camera.getPicture(onSuccess, onFail, {
+            quality: 50,
+            destinationType: Camera.DestinationType.DATA_URL,
+            sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+            correctOrientation: true
+        });
+
+        function onSuccess(imageData) {
+            var image = document.getElementById('pic');
+            image.src = "data:image/jpeg;base64," + imageData;
+        }
+
+        function onFail(message) {
+            alert('Failed because: ' + message);
+        }
+    }
+};
+
+app.initialize();
