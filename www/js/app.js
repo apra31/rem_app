0471d9a778e1345169595b2c064a1b42798d3493
Unknown <a.pra31@gmail.com>
2019-07-06 20:12:21 +0700

    Update homepage.

--- www/js/app.js
+++ www/js/app.js
@@ -8,11 +8,22 @@ var app = {
             window.location = "otp_mobile.html";
         }
         document.getElementById('registration-btn').addEventListener('click', this.onRegistrationButtonClicked.bind(this));
+        document.getElementById('menu-btn').addEventListener('click', this.onMenuButtonClicked.bind(this));
     },
 
     onRegistrationButtonClicked: function() {
         console.log('registration-btn is clicked')
         window.location = "registration.html";
+    },
+
+    onMenuButtonClicked: function() {
+        if(document.getElementById('menu-btn').classList.contains('button--close')) {
+            document.getElementById('menu-btn').classList.remove('button--close')
+            document.getElementById('secondary-menu').style.display = 'none';
+        } else {
+            document.getElementById('menu-btn').classList.add('button--close')
+            document.getElementById('secondary-menu').style.display = 'block';
+        }
     }
 };
 
