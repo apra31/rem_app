60c10c44838229a41aefffa454cbf05e147d00a3
Unknown <a.pra31@gmail.com>
2019-07-06 22:02:45 +0700

    Adjusts back & menu buttons in picture and registration pages.

--- www/js/picture.js
+++ www/js/picture.js
@@ -1,9 +1,20 @@
 var app = {
     // Application Constructor
     initialize: function() {
+        document.getElementById('menu-btn').addEventListener('click', this.onMenuButtonClicked.bind(this));
         document.getElementById('camera-btn').addEventListener('click', this.onCameraButtonClicked.bind(this));
         document.getElementById('gallery-btn').addEventListener('click', this.onGalleryButtonClicked.bind(this));
         document.getElementById('back-btn').addEventListener('click', this.onBackButtonClicked.bind(this));
+    },
+
+    onMenuButtonClicked: function() {
+        if(document.getElementById('menu-btn').classList.contains('button--close')) {
+            document.getElementById('menu-btn').classList.remove('button--close')
+            document.getElementById('secondary-menu').style.display = 'none';
+        } else {
+            document.getElementById('menu-btn').classList.add('button--close')
+            document.getElementById('secondary-menu').style.display = 'block';
+        }
     },
 
     onBackButtonClicked: function() {
--- www/js/registration.js
+++ www/js/registration.js
@@ -1,8 +1,19 @@
 var app = {
     // Application Constructor
     initialize: function() {
+        document.getElementById('menu-btn').addEventListener('click', this.onMenuButtonClicked.bind(this));
         document.getElementById('continue-btn').addEventListener('click', this.onContinueButtonClicked.bind(this));
         document.getElementById('back-btn').addEventListener('click', this.onBackButtonClicked.bind(this));
+    },
+
+    onMenuButtonClicked: function() {
+        if(document.getElementById('menu-btn').classList.contains('button--close')) {
+            document.getElementById('menu-btn').classList.remove('button--close')
+            document.getElementById('secondary-menu').style.display = 'none';
+        } else {
+            document.getElementById('menu-btn').classList.add('button--close')
+            document.getElementById('secondary-menu').style.display = 'block';
+        }
     },
 
     onBackButtonClicked: function() {
