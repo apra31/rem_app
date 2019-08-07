95aa59c0562a7f4d7b7af4061ffb6ff04fa88c53
Aditya Pradiptadarma <a.pra31@gmail.com>
2019-08-07 20:09:13 +0700

    Build android.

--- platforms/android/app/src/main/assets/www/js/home.js
+++ platforms/android/app/src/main/assets/www/js/home.js
@@ -2,6 +2,13 @@ var home = {
     // Application Constructor
     initialize: function() {
         document.getElementById('jadwal-btn').addEventListener('click', this.onJadwalButtonClicked.bind(this));
+        var url = 'http://gbi-rem.unchannels.com/api/v1/latest_worship_date';
+        var response = httpGet(url);
+        var latest_worship_date = JSON.parse(response).latest_worship_date;
+        if(latest_worship_date) {
+            document.getElementById('schedule-home-date').innerHTML = latest_worship_date
+            document.getElementById('schedule-home').style.display = 'block';
+        }
     },
 
     onJadwalButtonClicked: function() {
@@ -9,5 +16,12 @@ var home = {
         window.location = "jadwal.html";
     }
 };
+
+function httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
 
 home.initialize();
